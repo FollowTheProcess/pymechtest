457d53bee4c5810237a4bb9f03b19ea62218f0fd
Tom Fleet <tomfleet2018@gmail.com>
2020-12-31 09:17:03 +0000

    Made project PEP517 compliant

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,5 @@
 """
-Nox configuration file for the project
-
-Author: Tom Fleet
-Created: 08/12/2020
+Nox configuration file for the project.
 """
 
 from pathlib import Path
@@ -11,7 +8,7 @@ import nox
 
 PROJECT_ROOT = Path(__file__).parent.resolve()
 
-# nox defaults to virtualenv which is now deprecated
+# Nox defaults to virtualenv which is now deprecated
 nox.options.default_venv_backend = "venv"
 
 
@@ -21,8 +18,7 @@ def test(session):
     Runs the test suite against all supported python versions.
     """
     session.install("--upgrade", "pip", "setuptools", "wheel")
-    session.install(".")
-    session.install("pytest", "pytest-cov")
+    session.install(".[test]")
     # Posargs allows passing of tests directly
     tests = session.posargs or ["tests/"]
     session.run("pytest", "--cov=pymechtest", *tests)
@@ -34,7 +30,8 @@ def coverage(session):
     Test coverage analysis.
     """
     session.install("--upgrade", "pip", "setuptools", "wheel")
-    session.install("coverage")
+    session.install(".[cov]")
+
     session.run("coverage", "report", "--show-missing")
     session.run("coverage", "erase")
 
@@ -45,7 +42,7 @@ def lint(session):
     Formats project with black and isort, then runs flake8 and mypy linting.
     """
     session.install("--upgrade", "pip", "setuptools", "wheel")
-    session.install("black", "isort", "flake8", "mypy")
+    session.install(".[lint]")
     session.run("isort", ".")
     session.run("black", ".")
     session.run("flake8", ".")
@@ -58,7 +55,6 @@ def docs(session):
     Builds the project documentation.
     """
     session.install("--upgrade", "pip", "setuptools", "wheel")
-    session.install("mkdocs", "mkdocs-material", "mkdocstrings")
-    session.install(".")
+    session.install(".[docs]")
 
     session.run("mkdocs", "build", "--clean")
--- pymechtest/__init__.py
+++ pymechtest/__init__.py
@@ -1,8 +1,5 @@
 from pymechtest.tensile import Tensile
 
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
+__version__ = "0.1.0"
 
 __all__ = ["Tensile"]
