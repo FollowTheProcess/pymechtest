adb4b90e2da602cbd9a9780b64e94b688e3758e1
Tom Fleet <tomfleet2018@gmail.com>
2020-12-31 14:51:38 +0000

    Added skeleton classes for other test methods

--- pymechtest/__init__.py
+++ pymechtest/__init__.py
@@ -1,5 +1,8 @@
+from pymechtest.compression import Compression
+from pymechtest.flexure import Flexure
+from pymechtest.shear import Shear
 from pymechtest.tensile import Tensile
 
 __version__ = "0.1.0"
 
-__all__ = ["Tensile"]
+__all__ = ["Tensile", "Compression", "Flexure", "Shear"]
