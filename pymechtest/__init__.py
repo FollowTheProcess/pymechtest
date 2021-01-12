2b1368737042e51e29c63ccb6799285f84236627
Tom Fleet <tomfleet2018@gmail.com>
2021-01-12 14:13:27 +0000

    Bump version: 0.1.2 → 0.1.3

--- pymechtest/__init__.py
+++ pymechtest/__init__.py
@@ -3,6 +3,6 @@ from pymechtest.flexure import Flexure
 from pymechtest.shear import Shear
 from pymechtest.tensile import Tensile
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 __all__ = ["Tensile", "Compression", "Flexure", "Shear"]
